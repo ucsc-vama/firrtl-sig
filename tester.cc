19d3c1ee387339472ce113816e7b75ead6d53e00
Scott Beamer <sbeamer@lbl.gov>
2017-01-12 16:39:46 -0800

    cat operator tests

--- tester.cc
+++ tester.cc
@@ -62,6 +62,19 @@ const lest::test spec[] = {
     EXPECT( a16.pad<64>() == UInt<64>(0xcafe) );
     EXPECT( a64.pad<200>() == a64.pad<200>() );
     EXPECT( a64.pad<200>() != b64.pad<200>() );
+  },
+
+  CASE("cat operator") {
+    EXPECT( a16.cat(b16) == UInt<32>(0xcafebebe) );
+    EXPECT( a16.cat(a64) == UInt<80>("0xcafee2bd5b4ff8b30fc8") );
+    EXPECT( a64.cat(a16) == UInt<80>("0xe2bd5b4ff8b30fc8cafe") );
+    EXPECT( a16.cat(a80) == UInt<96>("0xcafe987426c1f7cd7d4d693a") );
+    EXPECT( a80.cat(a16) == UInt<96>("0x987426c1f7cd7d4d693acafe") );
+    EXPECT( a16.cat(a128) == UInt<144>("0xcafee903646a697fcaa344d2b2aa95e47b5d") );
+    EXPECT( a128.cat(a16) == UInt<144>("0xe903646a697fcaa344d2b2aa95e47b5dcafe") );
+    EXPECT( a80.cat(a128) == UInt<208>("0x987426c1f7cd7d4d693ae903646a697fcaa344d2b2aa95e47b5d") );
+    EXPECT( a128.cat(a80) == UInt<208>("0xe903646a697fcaa344d2b2aa95e47b5d987426c1f7cd7d4d693a") );
+    EXPECT( a128.cat(b128) == UInt<256>("0xe903646a697fcaa344d2b2aa95e47b5d56fa570ecb04adca42405f12bf28b822") );
   }
 };
 

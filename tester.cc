9a590ca30421c67b45328ec1721ac13c012ef0d3
Scott Beamer <sbeamer@lbl.gov>
2017-01-12 16:16:08 -0800

    tests of comparison operators

--- tester.cc
+++ tester.cc
@@ -2,14 +2,51 @@
 
 #include "uint.h"
 
-UInt<16> a(0xcafe);
-UInt<16> b(0xbebe);
+
+
+UInt<16>  a16(0xcafe);
+UInt<16>  b16(0xbebe);
+UInt<64>  a64(0xe2bd5b4ff8b30fc8);
+UInt<64>  b64(0x2fc353e33c6938a7);
+UInt<80>  a80("0x987426c1f7cd7d4d693a");
+UInt<80>  b80("0x563a0757a07b7bd27485");
+UInt<128> a128("0xe903646a697fcaa344d2b2aa95e47b5d");
+UInt<128> b128("0x56fa570ecb04adca42405f12bf28b822");
+
 
 const lest::test spec[] = {
-  CASE("hello")
-  {
-    EXPECT( a == a );
-    EXPECT( a != b );
+  CASE("comparison operators") {
+    EXPECT( a16 == a16 );
+    EXPECT( a16 != b16 );
+    EXPECT( b16 <  a16 );
+    EXPECT( b16 <= a16 );
+    EXPECT( a16 <= a16 );
+    EXPECT( a16 >= a16 );
+    EXPECT( a16 >  b16 );
+
+    EXPECT( a64 == a64 );
+    EXPECT( a64 != b64 );
+    EXPECT( b64 <  a64 );
+    EXPECT( b64 <= a64 );
+    EXPECT( a64 <= a64 );
+    EXPECT( a64 >= a64 );
+    EXPECT( a64 >  b64 );
+
+    EXPECT( a80 == a80 );
+    EXPECT( a80 != b80 );
+    EXPECT( b80 <  a80 );
+    EXPECT( b80 <= a80 );
+    EXPECT( a80 <= a80 );
+    EXPECT( a80 >= a80 );
+    EXPECT( a80 >  b80 );
+
+    EXPECT( a128 == a128 );
+    EXPECT( a128 != b128 );
+    EXPECT( b128 <  a128 );
+    EXPECT( b128 <= a128 );
+    EXPECT( a128 <= a128 );
+    EXPECT( a128 >= a128 );
+    EXPECT( a128 >  b128 );
   }
 };
 
